04d46a0032ddc853772a3fe32eefed98dbcfe389
Izzat Tulayev <izzatullohman@gmail.com>
2023-07-26 16:59:44 +0500

    fd

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -1,21 +1,21 @@
 package school.mjc.stage0.loops.task1;
 
-class Main {
+public class RepeatIsBad {
     public void repeatIsBad() {
         printMessage(20);
     }
 
-    private void printMessage(int remainingTimes) {
-        if (remainingTimes > 0) {
+    private void printMessage(int times) {
+        if (times <= 0) {
+            return;
+        } else {
             System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
-            remainingTimes--;
-            printMessage(remainingTimes);
+            printMessage(times - 1);
         }
     }
 
     public static void main(String[] args) {
-        Main main = new Main();
+        RepeatIsBad main = new RepeatIsBad();
         main.repeatIsBad();
     }
 }
-
